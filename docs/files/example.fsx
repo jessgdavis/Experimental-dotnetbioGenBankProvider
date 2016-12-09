da17994370f1dce9be6ae5cdfc2062538702a5dd
jess_d <j38.davis@connect.qut.edu.au>
2016-12-09 23:45:03 +1000

    Adding sample files

---
+++ docs/files/example.fsx
@@ -0,0 +1,8 @@
+#I "Z:\Users\Jess\git\Experimental-dotnetbioGenBankProvider"
+
+#r "bin\Experimental-dotnetbioGenBankProvider\Bio.Core.dll"
+#r "bin\Experimental-dotnetbioGenBankProvider\Experimental-dotnetbioGenBankProvider.dll"
+
+type ``NC_012686-Chlamydia trachomatis`` = Bio.FSharp.Experimental.GenBankProvider<"Z:\Users\Jess\git\dotnetbio-fsharp-project\Data\NC_012686-Chlamydia trachomatis.gbk">
+
+``NC_012686-Chlamydia trachomatis``.Accession
