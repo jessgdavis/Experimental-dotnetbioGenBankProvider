683f843802b56f4559a5d7a49777cbe706e7aaf1
jess_d <j38.davis@connect.qut.edu.au>
2016-12-13 11:18:59 +1000

    Update to sample script.

--- docs/files/example.fsx
+++ docs/files/example.fsx
@@ -1,13 +1,12 @@
-#I "Z:\Users\Jess\git\Experimental-dotnetbioGenBankProvider\src\Experimental-dotnetbioGenBankProvider"
+#I "../../bin/Experimental-dotnetbioGenBankProvider"
 
-#r "bin\debug\Bio.Core.dll"
-#r "bin\debug\Experimental-dotnetbioGenBankProvider.dll"
+#r "Bio.Core.dll"
+#r "Experimental-dotnetbioGenBankProvider.dll"
 
-type ``NC_012686-Chlamydia trachomatis`` = Bio.FSharp.Experimental.GenBankProvider< @"Z:\Users\Jess\git\Experimental-dotnetbioGenBankProvider\docs\files\NC_012686-Chlamydia trachomatis.gbk" >
+type ``NC_012686-Chlamydia trachomatis`` = Bio.FSharp.Experimental.GenBankProvider< @"NC_012686-Chlamydia trachomatis.gbk" >
 
 ``NC_012686-Chlamydia trachomatis``.``.NET Bio Objects``.GenBankMetaData
 
 ``NC_012686-Chlamydia trachomatis``.Accession
-
 
 ``NC_012686-Chlamydia trachomatis``.Version
