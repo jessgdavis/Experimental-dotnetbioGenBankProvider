3f9eacfb4c73f7485c02b4513c6b18257c78062e
jess_d <j38.davis@connect.qut.edu.au>
2016-12-13 10:25:34 +1000

    Tried passing byte list around, but does not appear to work (unsupported constant type).

--- docs/files/example.fsx
+++ docs/files/example.fsx
@@ -1,8 +1,13 @@
-#I "Z:\Users\Jess\git\Experimental-dotnetbioGenBankProvider"
+#I "Z:\Users\Jess\git\Experimental-dotnetbioGenBankProvider\src\Experimental-dotnetbioGenBankProvider"
 
-#r "bin\Experimental-dotnetbioGenBankProvider\Bio.Core.dll"
-#r "bin\Experimental-dotnetbioGenBankProvider\Experimental-dotnetbioGenBankProvider.dll"
+#r "bin\Debug\Bio.Core.dll"
+#r "bin\Debug\Experimental-dotnetbioGenBankProvider.dll"
 
-type ``NC_012686-Chlamydia trachomatis`` = Bio.FSharp.Experimental.GenBankProvider<"Z:\Users\Jess\git\dotnetbio-fsharp-project\Data\NC_012686-Chlamydia trachomatis.gbk">
+let ref = @"Z:\Users\Jess\git\Experimental-dotnetbioGenBankProvider\docs\files\NC_012686-Chlamydia trachomatis.gbk"
+type ``NC_012686-Chlamydia trachomatis`` = Bio.FSharp.Experimental.GenBankProvider< @"Z:\Users\Jess\git\Experimental-dotnetbioGenBankProvider\docs\files\NC_012686-Chlamydia trachomatis.gbk" >
 
-``NC_012686-Chlamydia trachomatis``.Accession
+``NC_012686-Chlamydia trachomatis``.Comments
+
+
+
+``NC_012686-Chlamydia trachomatis``.``.NET Bio Objects``.ISequence
